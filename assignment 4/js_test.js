cc317ef1043a90a7739e09d49157c1ced3805c79
EwanSeaders <122401941+EwanSeaders@users.noreply.github.com>
2024-03-23 12:30:50 -0700

    assignment 4 commit

---
+++ assignment 4/js_test.js
@@ -0,0 +1,23 @@
+// this is my first js code
+
+// let name = "poopy";
+const inrerest_rate = 0.1;
+let person = {
+    name: 'ted',
+    age: 30
+
+};
+
+function greet(name) {
+    console.log(f'hello "')
+}
+
+greet('polly');
+// console.log(typof(inrerest_rate));
+
+function factorial(num) {
+    if num != 0;
+        
+    else 
+}
+console.log(person);
